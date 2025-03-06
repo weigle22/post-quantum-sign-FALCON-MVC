0d6505940fdbe955e7fe7985e22d62214fbff253
weigle22 <emmanuelotacan95@gmail.com>
2025-03-06 08:05:21 +0800

    removed unecessary codes

--- FALCONx/FALCONx/Controllers/HomeController.cs
+++ FALCONx/FALCONx/Controllers/HomeController.cs
@@ -37,13 +37,6 @@ namespace FALCONx.Controllers
             } 
             else
             {
-                //var clientId = "128694056230-81hbfpiprlfgr0g7f261ii0r9q5f5q4b.apps.googleusercontent.com";
-                //var url = "https://localhost:44335/Home/About";
-                //var clientsecret = "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";
-                //var token = await GoogleAuth.GetAuthAccessToken(code, clientId, clientsecret, url);
-
-                //var userProfile = await GoogleAuth.GetProfileResponseAsync(token.AccessToken.ToString());
-                //ViewBag.Message = userProfile.ToString();
                 return View();
             }
 
